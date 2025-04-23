f2565146c98689436b9cfb56d2f0d74531119dd5
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2025-04-23 08:28:16 -0300

    Criados metodos de CRUD de produto em produto_model

--- padrao_model_view_controller/padrao_model_view_controller_segundo_exemplo/models/produto_model.py
+++ padrao_model_view_controller/padrao_model_view_controller_segundo_exemplo/models/produto_model.py
@@ -20,3 +20,33 @@ class Produto:
         """
         _executar(query)
         
+
+    def salvar(self):
+        query = f"INSERT INTO produtos (nome, preco, status) VALUES ('{self.nome}', {float(self.preco)}, {int(self.status)})"
+        _executar(query)
+
+
+    def atualizar(self):
+        query = f"UPDATE produtos SET status = {int(self.status)} WHERE id = {int(self.id)}"
+        _executar(query)
+
+
+    def deletar(self):
+        query = f"DELETE FROM produtos WHERE id = {int(self.id)}"
+        _executar(query)
+
+
+    @staticmethod
+    def get_produtos():
+        query = "SELECT * FROM produtos"
+        produtos = _executar(query)
+        return produtos
+    
+
+    @staticmethod
+    def get_produto(id):
+        query = f"SELECT * FROM produtos WHERE id = {int(id)} "
+        produto = _executar(query)[0]
+        produto = Produto(id=produto[0], nome=produto[1], preco=produto[2])
+        return produto
+    
